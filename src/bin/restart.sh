d3c5fcc16b36888ae06c26c8dfecfe96b479f0c6
Young-Pastor <young@zhisida.com>
2022-07-03 08:29:28 +0000

    update src/bin/restart.sh.

--- src/bin/restart.sh
+++ src/bin/restart.sh
@@ -3,10 +3,11 @@
 # 项目名称
 APPLICATION="Young-Board"
 
+BIN_PATH=$(cd `dirname $0`; pwd)
 # 停服
 echo stop ${APPLICATION} Application...
-sh shutdown.sh
+sh ${BIN_PATH}/shutdown.sh
 
 # 启动服务
 echo start ${APPLICATION} Application...
-sh startup.sh
+sh ${BIN_PATH}/startup.sh
